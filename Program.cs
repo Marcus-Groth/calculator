5de6e2ec2c69d7975df26ba753c9094eff29caea
rodersnow <rodersnow@gmail.com>
2024-08-07 10:09:38 +0200

    feat(Program): add OperationsMenu method to display basic operations menu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Calculator
 {
@@ -6,7 +6,22 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Testing this app");
+            Console.Clear();
+            OperationsMenu();
+
+
+        static public void OperationsMenu() 
+        {
+            // Create Input Menu for a calulator
+            Console.WriteLine("Basic Operations");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.Write("Please select an operation: ");
+        }
+
+        }
         }
     }
 }
